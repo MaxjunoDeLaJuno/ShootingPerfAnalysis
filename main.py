affcf836ee4cde7916f27dc6a8abdbc6f8ac4f63
MaxjunoDeLaJuno <Maxjuno24@gmail.com>
2025-12-16 08:22:26 +0100

    ajout des prototypes de fonctions get et write dans le main.py

--- main.py
+++ main.py
@@ -20,6 +20,17 @@ def log(message):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"[{timestamp}] {message}")
 
+# get an object <ShootingData> from ""
+def getShootingData():
+    pass
+    
+# get an object <ShootingPerf> from ""
+def getShootingPerf():
+    pass
+
+# write the object <ShootingPerf> as JSON file in ""
+def saveShootingPerf():
+    pass
 
 if __name__ == "__main__":
     log(f"starting of {CONFIG['app_name']} v{CONFIG['version']}")
